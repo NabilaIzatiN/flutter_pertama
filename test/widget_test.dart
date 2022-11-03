856f6493ff2200c93403024b0b982a7f7c5bc394
Nabila <nabila.izatin22.12.2000@gmail.com>
2022-11-03 23:17:20 +0700

    commit conditional

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -75,4 +75,19 @@ void main() {
   print(a < b);
   print(a >= b);
   print(a <= b);
+
+  //conditional
+  var nilai;
+  nilai = 90;
+
+  if (nilai > 80) {
+    print("A");
+  } else if (nilai <= 80 && nilai >= 50) {
+    print("B");
+  } else {
+    print("Tidak Lulus");
+  }
+
+  print("-------------");
+  nilai >= 80 ? print("A") : print("tidak A");
 }

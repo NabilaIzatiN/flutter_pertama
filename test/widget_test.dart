97127edac67973e552e0efeba9b4eb040aac103d
Nabila <nabila.izatin22.12.2000@gmail.com>
2022-11-03 19:38:05 +0700

    commit operator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -22,4 +22,57 @@ void main() {
   mahasiswastring = "Jay";
 
   print(mahasiswastring);
+
+  //int
+  int semester;
+  semester = 6;
+  print(semester);
+
+  //double
+  double ipk;
+  ipk = 3.7;
+
+  print(ipk);
+
+  //boolean
+  bool benar = true;
+  bool salah = false;
+  bool tidakbenar = !true;
+  bool tidaksalah = !false;
+
+  //list
+  List jurusan = [
+    "Teknik Informatika",
+    "Sistem Informasi",
+    "DKV",
+    semester.toString(),
+    ipk.toString()
+  ];
+  print(jurusan);
+
+  //map
+  Map<String, dynamic> kelas = {
+    "nama": "Hesa",
+    "kelas": "TI 1",
+  };
+
+  print(kelas);
+  print(kelas['nama']);
+  print(kelas['kelas']);
+
+  //Operator
+  int a, b;
+  a = 9;
+  b = 7;
+  print("a = " + a.toString());
+  print("b = " + b.toString());
+
+  print(a + b);
+  print(a - b);
+  print(a / b);
+  print(a * b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
 }

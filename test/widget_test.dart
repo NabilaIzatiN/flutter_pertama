aeaa8fc44cdbb095280fd52d1f668bf4ab4d4bfb
Nabila <nabila.izatin22.12.2000@gmail.com>
2022-11-03 19:32:40 +0700

    commit operator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pertama/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  //tipe data dan variable
+  var mahasiswa = "Sean";
+  var umur = "20";
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  print(mahasiswa + " umur= " + umur);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //string
+  String mahasiswastring = "Jaya";
+  mahasiswastring = "Jay";
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  print(mahasiswastring);
 }
